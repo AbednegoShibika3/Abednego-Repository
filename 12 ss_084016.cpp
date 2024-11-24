1adb45b68bee598eab90814876fea383cebedda2
Abednego Shibika 3 <abednegoshibika@gmail.com>
2024-11-24 15:45:00 +0300

    My first commit

---
+++ 12 ss_084016.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Enter the digit u wish";
+	int limit;
+	
+	cin>>limit;
+	
+	for (int i=limit; i<10; i++)
+	     cout<<"i="<<i<<"\n";
+	cout<<"C++ knows where to stop";
+}
