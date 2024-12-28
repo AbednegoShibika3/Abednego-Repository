f5cd5221fc0c30ce853a6ead62c3895f409c6bd0
Abednego Shibika 3 <abednegoshibika@gmail.com>
2024-12-28 23:02:14 +0300

    Factorial of a number

---
+++ Factorial of number (2nd Technique)_084651.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no, f=1;
+	
+	cout<<"Enter any number";
+	cin>>no;
+	
+	for(int i = 1; i<=no; i++)
+	{
+	f = f*1;
+	}
+	cout<<"factorial="<<f;
+}
